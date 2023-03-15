d5b0e57c703af87f223c435da5c3311e0303a651
marsik-ms <79077490+marsik-ms@users.noreply.github.com>
2023-03-15 13:14:11 +0200

    Initial Commit

---
+++ MyHomeWork1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHomeWork1
+//
+//  Created by Stanislav Perepechin on 15/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
